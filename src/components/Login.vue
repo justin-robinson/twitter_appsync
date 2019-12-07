ccb56a6a6740cb18b1cdc6db50afcca209ba2faf
Justin Robinson <jurobn@amazon.com>
2019-12-07 14:14:06 -0500

    create login page

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Login">
+    <amplify-sign-in v-bind:signInConfig="signInConfig"></amplify-sign-in>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { AmplifyEventBus } from "aws-amplify-vue";
+
+@Component
+export default class Login extends Vue {
+  data() {
+    return {
+      signInConfig: {
+        username: "username"
+      }
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
