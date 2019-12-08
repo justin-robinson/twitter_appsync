47a7d6dba4d40c7dde99896a185b912d4d0d3590
Justin Robinson <jurobn@amazon.com>
2019-12-07 23:03:17 -0500

    fix possibly null value error preventing yarn build

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { get } from "lodash";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-      user: null
+      user: null,
+      isLoggedIn: false
   },
   mutations: {
       setUser(state, user) {
           state.user = user;
+          state.isLoggedIn = true;
       }
   },
   getters: {
-      isLoggedIn(state) {
-          return state.user !== null
+      isLoggedIn(state): boolean {
+          return state.isLoggedIn;
       },
-      firstName(state) {
-          return state.user.attributes ? state.user.attributes.given_name : '';
+      firstName(state): string {
+        return get(state, 'user.attributes.given_name');
       }
   },
   actions: {},
